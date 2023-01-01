2ef035137dcb3e0f2fb429acf1f5548ca46aeb39
Anna <annkrv80@yandex.ru>
2023-01-01 21:49:07 +0700

    Закрытие шкафа

---
+++ CloseCupboard.java
@@ -0,0 +1,15 @@
+public class CloseCupboard {
+
+    public void close(Cupboard cup, Man name){
+        System.out.println(name.getName() + "  Закрой шкаф!!!!");
+        if(cup.getLock()==name.getKey()){
+            cup.setStatus("закрыт");
+            System.out.println("Шкаф № "+cup.getNumber() + " " + cup.getStatus());
+        }
+        else{
+            System.out.println("Ключ неподходит!!!");
+        }
+    }
+    
+    
+}
