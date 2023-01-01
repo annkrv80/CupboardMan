00347201568c14c659b8ddb0af24e5f6898d5c3a
Anna <annkrv80@yandex.ru>
2023-01-01 21:51:33 +0700

    Main

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Cupboard cupboard=new Cupboard(1);
+        cupboard.setStatus("закрыт");
+        System.out.println(cupboard);
+        Man andrey=new Man("Андрей");
+        
+
+        if(cupboard.getStatus()=="закрыт"){
+            OpenCupboard openCupboard=new OpenCupboard();
+            openCupboard.open(cupboard, andrey);
+        }
+        else if (cupboard.getStatus()=="открыт"){
+            CloseCupboard closeCupboard=new CloseCupboard();
+            closeCupboard.close(cupboard, andrey);
+        }
+     
+    }
+}
