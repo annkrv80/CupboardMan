603b8718d0a7b570bd5afc0ab5550eb4089b8757
Anna <annkrv80@yandex.ru>
2023-01-01 21:49:43 +0700

    Шкаф

---
+++ Cupboard.java
@@ -0,0 +1,36 @@
+public class Cupboard{
+    private String status;
+    private int number;
+    private int  lock ;
+
+    public Cupboard(int number){
+        this.number=number;     
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public int getLock() {
+        return lock;
+    }
+
+    public void setLock(int lock) {
+        this.lock = lock;
+    }
+    @Override
+    public String toString() {
+        
+        return "Шкаф № " + this.number + " "+ this.status;
+    }
+
+
+}
