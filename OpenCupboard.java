f3b7f76419d4cdb21d555acba6d9964f0ce5d235
Anna <annkrv80@yandex.ru>
2023-01-05 22:27:25 +0700

    Открой шкаф

--- OpenCupboard.java
+++ OpenCupboard.java
@@ -2,12 +2,11 @@ public class OpenCupboard {
 
     public void open(Cupboard cup, Man name) {
         System.out.println(name + " открой шкаф!");
-        if (cup.getLock() == name.getKey()) {
-            System.out.println("Ура!!! Ключ найден!!!");
+        if (name.getKey()=="Есть") {
             cup.setStatus("открыт");
             System.out.println("Шкаф № " + cup.getNumber() + " " + cup.getStatus());
         } else {
-            System.out.print("Упс...Ключ неподходит! Попробуй еше раз!");
+            System.out.print("Упс...нет ключа!");
         }
 
     }
